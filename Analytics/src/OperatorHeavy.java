c66fdbf836c1495d92b8b8e1e628bc111e92d246
SenneRosaer <33682771+SenneRosaer@users.noreply.github.com>
2023-05-30 16:15:58 +0200

    Update OperatorHeavy.java

--- Analytics/src/OperatorHeavy.java
+++ Analytics/src/OperatorHeavy.java
@@ -41,5 +41,56 @@ public class OperatorHeavy {
                 (ch >= '\u2028' && ch <= '\u202F') || (ch >= '\u2060' && ch <= '\u206F') ||
                 (ch >= '\uFFF0') || (ch >= '\uD800' && ch <= '\uDFFF');
     }
+    
+    public static boolean areBoxingCompatible(String desc1, String desc2) {
+        if (desc1.equals(desc2)) {
+            return true;
+           }
+        if (desc1.length() == 1) {
+            if (desc1.equals("Z")) {
+                return desc2.equals("Ljava/lang/Boolean");
+            }
+            else if (desc1.equals("D")) {
+                return desc2.equals("Ljava/lang/Double");
+            }
+            else if (desc1.equals("F")) {
+                return desc2.equals("Ljava/lang/Float");
+            }
+            else if (desc1.equals("I")) {
+                return desc2.equals("Ljava/lang/Integer");
+            }
+            else if (desc1.equals("J")) {
+                return desc2.equals("Ljava/lang/Long");
+            }
+        }
+        else if (desc2.length() == 1) {
+            if (desc2.equals("Z")) {
+                return desc1.equals("Ljava/lang/Boolean");
+            }
+            else if (desc2.equals("D")) {
+                return desc1.equals("Ljava/lang/Double");
+            }
+            else if (desc2.equals("F")) {
+                return desc1.equals("Ljava/lang/Float");
+            }
+            else if (desc2.equals("I")) {
+                return desc1.equals("Ljava/lang/Integer");
+            }
+            else if (desc2.equals("J")) {
+                return desc1.equals("Ljava/lang/Long");
+            }
+        }
+        return false;
+    }
 
+   private static int nextPowerOf2(int val) {
+        val--;
+        val = (val >> 1) | val;
+        val = (val >> 2) | val;
+        val = (val >> 4) | val;
+        val = (val >> 8) | val;
+        val = (val >> 16) | val;
+        val++;
+        return val;
+    }
 }
